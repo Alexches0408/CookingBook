33c5bf9232ff94f52b685030595f3ee0af684bbf
Alexches0408 <103947115+Alexches0408@users.noreply.github.com>
2023-04-25 16:48:07 +0800

    Add files via upload

--- menu/models.py
+++ menu/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=120, unique=True)
@@ -11,15 +12,17 @@ class Dishe(models.Model):
     name = models.CharField(max_length=120, unique=True)
     products = models.ManyToManyField(Product, through='ProductDishe')
     number_of_person = models.IntegerField(default=1)
-    steps = models.TextField()
+    steps = models.TextField(max_length=200)
 
     def __str__(self):
         return f'{self.name}'
 
+    
+
 
 class ProductDishe(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    dishe = models.ForeignKey(Dishe, on_delete=models.CASCADE)
+    dishe = models.ForeignKey(Dishe, on_delete=models.CASCADE, related_name = 'proddish',)
     amount = models.IntegerField(default=1)
     thing = 'th'
     cup = 'cup'
--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView
-from .models import Product, Dishe
+from .models import Product, Dishe, ProductDishe
 from .forms import AddDisheForm
 # Create your views here.
 
@@ -9,8 +9,29 @@ class ProductList(ListView):
     template_name = 'menu/dishes.html'
     context_object_name = 'dishes'
 
+
 class CreateDish(CreateView):
+    model = Dishe
     template_name = 'menu/add_dish.html'
     form_class = AddDisheForm
 
+    def post(self, request, *args, **kwargs):
+        # берём значения для нового товара из POST-запроса отправленного на сервер
+        form = self.form_class(request.POST) # создаём новую форму, забиваем в неё данные из POST-запроса 
+        products= request.POST['products']
+        amount = request.POST['amount']
+        name = request.POST['name']
+        prodamount = dict(zip(products,amount))
+ 
+        if form.is_valid(): # если пользователь ввёл всё правильно и нигде не накосячил, то сохраняем новый товар
+            form.save()
+        
+        PrDishes = ProductDishe.objects.filter(dishe_id=Dishe.objects.get(name = name).id)
 
+        for key, value in prodamount:
+            item = PrDishes.objects.get(product_id = key)
+            item.amount = value
+            item.save()
+
+ 
+        return super().get(request, *args, **kwargs)
