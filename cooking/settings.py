fe8440996cb5cab70cd49b174e38fd3753a16161
Alexches0408 <103947115+Alexches0408@users.noreply.github.com>
2023-06-27 08:01:31 +0800

    Update settings.py

--- cooking/settings.py
+++ cooking/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h2xwbn&jzd$9)0_zc39pwm95fq=_f4cjcw09wf%t%bidj3t^fi'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
